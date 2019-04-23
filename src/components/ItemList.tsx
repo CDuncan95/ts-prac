f2744f40ed47302f7b0ae01df1e6d8af73d65bc8
Chris Duncan <chrisduncan95@hotmail.co.uk>
2019-04-23 19:35:13 +0100

    Implement expanding/collapsing of descriptions

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { APIListItem } from '../apis/temp'
+import expand from '../resources/expand.png'
+import collapse from '../resources/collapse.png'
 import './ItemList.css';
 
 interface ItemListProps {
@@ -14,9 +16,9 @@ class ItemList extends Component<ItemListProps, ItemListState> {
     public render() {
         return (
             <ul className='ItemList'>
-                {this.props.listItems.map((listItem : APIListItem) => {
+                {this.props.listItems.map((listItem : APIListItem, index ) => {
                     console.log(listItem);
-                    return (<ListItem data={listItem}></ListItem>);
+                    return (<ListItem data={listItem} index={index} key={index} ></ListItem>);
                 })}
             </ul>
         );
@@ -24,21 +26,51 @@ class ItemList extends Component<ItemListProps, ItemListState> {
 };
 
 interface ListItemProps {
-    data: APIListItem
+    key: Number, // required for react
+    data: APIListItem,
+    index: Number
 };
 
 interface ListItemState {
-
+    descriptionCollapsed: boolean
 };
 
 class ListItem extends Component<ListItemProps, ListItemState> {
+
+    public constructor(props: ListItemProps) {
+        super(props);
+        this.state = {
+            descriptionCollapsed: true
+        };
+    }
+
     public render() {
         return (
-            <li className='ListItem'>
-                <p>{`${this.props.data.title}`}</p>
-                <button className={this.props.data.description ? "VisibleButton" : "HiddenButton"}>Description</button>
+            <li className="ListItem" id={`listItem${this.props.index}`}>
+                <div className="ListItemConstant">
+                    <p className="ListItemTitle">{`${this.props.data.title}`}</p>
+                    <img
+                        className={this.props.data.description ? 'clickable' : 'notClickable'}
+                        onClick={this.props.data.description ? this.toggleCollapsedContent.bind(this) : this.expanderNOOP.bind(this) }
+                        src={this.state.descriptionCollapsed ? expand : collapse }
+                    ></img>
+                </div>
+                <div 
+                    id={`collapsedContent${this.props.index}`}
+                    className={this.state.descriptionCollapsed ? 'collapsed' : 'expanded' }
+                >
+                    <p>{this.props.data.description || ""}</p>
+                </div>
             </li>
         );
+    }
+
+    private expanderNOOP() {}
+
+    private toggleCollapsedContent() {
+        this.setState({
+            descriptionCollapsed: !this.state.descriptionCollapsed
+        });
     }
 }
 
